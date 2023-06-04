dbb5d6293286b95f16d05facc2fe40b29afff417
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-06-05 02:43:05 +0700

    update pmpermit.py

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -212,9 +212,7 @@ async def pmpermit_func(client, message):
     if flood[str(org)] > 5:
         await message.reply_text("SPAM DETECTED, BLOCKED USER AUTOMATICALLY!")
         return await client.block_user(org)
-    results = await client.get_inline_bot_results(
-        app.me.username, f"pmpermit {org}", victim
-    )
+    results = await client.get_inline_bot_results(app.me.username, f"pmpermit {org}")
     await client.send_inline_bot_result(
         org,
         results.query_id,
@@ -279,7 +277,6 @@ async def pmpermit_cq(_, cq, victim):
             "I'm busy right now, will approve you shortly, DO NOT SPAM.",
         )
 
-
 @app.on_inline_query()
 async def pmpermit_func(answers, user_id, victim):
     if user_id != client.me.id:
@@ -304,6 +301,7 @@ async def pmpermit_func(answers, user_id, victim):
         ),
     )
     answers.append(
+        
         InlineQueryResultArticle(
             title="do_not_click_here",
             reply_markup=buttons,
