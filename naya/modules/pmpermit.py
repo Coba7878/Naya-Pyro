d0b3dfee2caf6f3e8d59e8352ab00bde7dca3bc7
naya1503 <naya1503@users.noreply.github.com>
2023-06-04 20:12:35 +0000

    Auto Fix

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -153,11 +153,7 @@ async def set_limit(client, message):
 
 
 @bots.on_message(
-    filters.private
-    & filters.incoming
-    & ~filters.service
-    & ~filters.me
-    & ~filters.bot
+    filters.private & filters.incoming & ~filters.service & ~filters.me & ~filters.bot
 )
 async def pmpermit_func(client, message):
     org = message.from_user.id
@@ -275,6 +271,7 @@ async def pmpermit_cq(_, cq):
             user_id,
             "I'm busy right now, will approve you shortly, DO NOT SPAM.",
         )
+
 
 @app.on_callback_query(filters.regex("pmpermit"))
 async def pmpermit_func(answers, user_id, victim):
