c625b61905bacc7c51a08adaf852c5ce655bfdc5
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-06-05 03:44:16 +0700

    update pmpermit.py

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -275,6 +275,7 @@ async def pmpermit_cq(_, cq):
 @app.on_inline_query(filters.regex("pmpermit"))
 async def pmpermit_func(client, query):
     await bot.get_me()
+    answers = []
     caption = "Hi, I'm {}, What are you here for?, You'll be blocked if you send more than 5 messages."
     buttons = InlineKeyboard(row_width=2)
     buttons.add(
@@ -286,7 +287,9 @@ async def pmpermit_func(client, query):
             callback_data="pmpermit to_scam_you a",
         ),
         InlineKeyboardButton(text="Approve me", callback_data="pmpermit approve_me a"),
-        InlineKeyboardButton(text="Approve", callback_data=f"pmpermit approve"),
+        InlineKeyboardButton(
+            text="Approve", callback_data=f"pmpermit approve"
+        ),
         InlineKeyboardButton(
             text="Block & Delete",
             callback_data=f"pmpermit block",
@@ -296,9 +299,7 @@ async def pmpermit_func(client, query):
         InlineQueryResultArticle(
             title="do_not_click_here",
             reply_markup=buttons,
-            input_message_content=InputTextMessageContent(
-                caption.format(bots.me.first_name)
-            ),
+            input_message_content=InputTextMessageContent(caption.format(bots.me.first_name)),
         )
     )
     await client.answer_inline_query(query.id, results=answers, cache_time=0)
