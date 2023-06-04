41f9595fd85f280065ab17e32e2a8c363d91dbf2
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-06-05 04:44:47 +0700

    update pmpermit.py

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -4,11 +4,9 @@
 # Kok Bacot
 # © @KynanSupport | Nexa_UB
 # FULL MONGO NIH JING FIX MULTI CLIENT
+from pyrogram.raw.functions.messages import DeleteHistory
 import traceback
 from gc import get_objects
-
-from pyrogram.raw.functions.messages import DeleteHistory
-
 from . import *
 
 PM_GUARD_WARNS_DB = {}
@@ -220,7 +218,6 @@ async def pmpermit(client, message):
 
 flood2 = {}
 
-
 @app.on_callback_query()
 async def pmpermit_cq(_, cq):
     user_id = cq.from_user.id
@@ -276,6 +273,7 @@ async def pmpermit_cq(_, cq):
         )
 
 
+
 async def pmpermit_func(message, answers, victim):
     loh = message._from_user.id
     gua = message._client.me.id
@@ -287,7 +285,7 @@ async def pmpermit_func(message, answers, victim):
     if loh != gua:
         return
     caption = f"Halo 👋 {siapa}, Saya adalah {gua2} ! Jangan spam pesan atau anda akan diblokir otomatis.\n\nAnda punya peringatan {flood[loh]}/{pm_limit} ."
-    get_pm if get_pm else caption
+    pm_text = get_pm if get_pm else caption
     buttons = InlineKeyboard(row_width=2)
     buttons.add(
         InlineKeyboardButton(
@@ -315,9 +313,8 @@ async def pmpermit_func(message, answers, victim):
     )
     return answers
 
-
 @app.on_inline_query()
-async def inline_query_handler(client, query):
+async def inline_query_handler(client, query, victim):
     try:
         text = query.query.strip().lower()
         answers = []
@@ -331,8 +328,8 @@ async def inline_query_handler(client, query):
     except Exception as e:
         e = traceback.format_exc()
         print(e, "InLine")
-
-
+        
+        
 __MODULE__ = "antipm"
 __HELP__ = f"""
 ✘ Bantuan Untuk PM Permit
