aaafe576892abd90279a89b48b9c5fbde11b6b40
naya1503 <naya1503@users.noreply.github.com>
2023-06-04 19:49:44 +0000

    Auto Fix

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -277,6 +277,7 @@ async def pmpermit_cq(_, cq, victim):
             "I'm busy right now, will approve you shortly, DO NOT SPAM.",
         )
 
+
 @app.on_inline_query(filters.regex("pmpermit"))
 async def pmpermit_func(answers, user_id, victim):
     if user_id != client.me.id:
