5517b8afecb868ac8dbed3bbbbdf03b9ce96f714
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-06-05 02:40:14 +0700

    update pmpermit.py

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -214,7 +214,7 @@ async def pmpermit_func(client, message):
         return await client.block_user(org)
     results = await client.get_inline_bot_results(app.me.username, f"pmpermit {org}")
     await client.send_inline_bot_result(
-        cache_time=300,
+      
         org,
         results.query_id,
         results.results[0].id,
