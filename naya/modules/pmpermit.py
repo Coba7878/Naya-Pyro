c5243047635298453d5289dc777c3a636c8187eb
naya1503 <naya1503@users.noreply.github.com>
2023-06-04 20:33:37 +0000

    Auto Fix

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -153,11 +153,7 @@ async def set_limit(client, message):
 
 
 @bots.on_message(
-    filters.private
-    & filters.incoming
-    & ~filters.service
-    & ~filters.me
-    & ~filters.bot
+    filters.private & filters.incoming & ~filters.service & ~filters.me & ~filters.bot
 )
 async def pmpermit(client, message):
     org = message.from_user.id
@@ -218,8 +214,8 @@ async def pmpermit(client, message):
         result_id=x.results[0].id,
     )
 
-flood2 = {}
 
+flood2 = {}
 
 
 async def pmpermit_cq(_, cq):
@@ -307,11 +303,7 @@ async def pmpermit_func(answers, user_id):
             input_message_content=InputTextMessageContent(caption),
         )
     )
-    await client.answer_inline_query(
-        query.id,
-        results=answers,
-        cache_time=0
-    )
+    await client.answer_inline_query(query.id, results=answers, cache_time=0)
 
 
 __MODULE__ = "antipm"
