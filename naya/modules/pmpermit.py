cc542a7c90c4f73ac8bbed7d30a8d5701634a0e0
naya1503 <naya1503@users.noreply.github.com>
2023-06-04 22:21:44 +0000

    Auto Fix

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -323,7 +323,9 @@ async def inline_query_handler(client, inline_query):
         if string.split()[0] == "pmpermit":
             m = [obj for obj in get_objects() if id(obj) == get_id][0]
             answers = await pmpermit_func(m, answers)
-            await client.answer_inline_query(inline_query.id, results=answers, cache_time=300)
+            await client.answer_inline_query(
+                inline_query.id, results=answers, cache_time=300
+            )
     except Exception as e:
         e = traceback.format_exc()
         print(e, "InLine")
