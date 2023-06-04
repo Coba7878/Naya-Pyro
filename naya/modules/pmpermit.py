066ec550d8d2f1879132586465f8e6cd8bccff77
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-06-05 05:35:37 +0700

    update pmpermit.py

--- naya/modules/pmpermit.py
+++ naya/modules/pmpermit.py
@@ -5,6 +5,7 @@
 # © @KynanSupport | Nexa_UB
 # FULL MONGO NIH JING FIX MULTI CLIENT
 import traceback
+from gc import get_objects
 
 from pyrogram.raw.functions.messages import DeleteHistory
 
@@ -320,10 +321,8 @@ async def inline_query_handler(client, inline_query):
         get_id = inline_query.query.split()
         answers = []
         if string.split()[0] == "pmpermit":
-            answers = await pmpermit_func(int(get_id[2]), answers)
-            await client.answer_inline_query(
-                inline_query.id, results=answers, cache_time=300
-            )
+            answers = await pmpermit_func(int(get_id[0]), answers)
+            await client.answer_inline_query(inline_query.id, results=answers, cache_time=300)
     except Exception as e:
         e = traceback.format_exc()
         print(e, "InLine")
